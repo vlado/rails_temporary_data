e42ceb2216210ce67099deaaad0e894fde4d7888
Vlado Cingel <vladocingel@gmail.com>
2012-03-09 16:18:41 +0100

    include Rails::Generators::Migration

--- lib/generators/rails_temporary_data_generator.rb
+++ lib/generators/rails_temporary_data_generator.rb
@@ -1,4 +1,8 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+
 class RailsTemporaryDataGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
   
   def create_migration_file
     migration_template 'migration.rb', 'db/migrate/create_temporary_data.rb'
