146605ef83bcc9562e52bf0633a6b6777b428129
Vlado Cingel <vladocingel@gmail.com>
2012-03-09 17:00:21 +0100

    next migration number implemented

--- lib/generators/rails_temporary_data_generator.rb
+++ lib/generators/rails_temporary_data_generator.rb
@@ -4,6 +4,10 @@ require 'rails/generators/migration'
 class RailsTemporaryDataGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
   
+  def self.next_migration_number
+    ActiveRecord::Migration.new.next_migration_number(1)
+  end
+  
   def create_migration_file
     migration_template 'migration.rb', 'db/migrate/create_temporary_data.rb'
   end
