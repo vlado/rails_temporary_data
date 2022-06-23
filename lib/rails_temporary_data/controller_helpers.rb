a973187a9f07f5e1368c02291e0dd49d41b8a08f
Vladimir <vulkan.u4u1@gmail.com>
2022-06-24 02:03:40 +0600

    Update controller_helpers.rb
    Disable use session

--- lib/rails_temporary_data/controller_helpers.rb
+++ lib/rails_temporary_data/controller_helpers.rb
@@ -2,30 +2,44 @@ module RailsTemporaryData
   module ControllerHelpers
     extend ActiveSupport::Concern
 
-    def set_tmp_data(key, data, expires_at = nil)
-      tmp_data = TemporaryData.create!(:data => data, :expires_at => expires_at)
-      session[key] = tmp_data.id
+    def set_tmp_data(key, data, expires_at = nil, use_session = false)
+      tmp_data = TemporaryData.create!(:data => data, :expires_at => expires_at, :key => key)
+      session[key] = tmp_data.id if use_session
     end
 
-    def get_tmp_data(key)
-      tmp_data = TemporaryData.unexpired.find_by_id(session[key])
-      session[key] = nil if tmp_data.nil?
+    def get_tmp_data(key, use_session = false)
+      tmp_data = if use_session
+        TemporaryData.unexpired.find_by_id(session[key])
+      else
+        TemporaryData.unexpired.find_by(key: key)
+      end
+   
+      session[key] = nil if tmp_data.nil? && use_session
       tmp_data
     end
 
-    def clear_tmp_data(key)
-      tmp_data = TemporaryData.unexpired.find_by_id(session[key])
-      session[key] = nil
+    def clear_tmp_data(key, use_session = false)
+      tmp_data = if use_session
+        TemporaryData.unexpired.find_by_id(session[key])
+      else
+        TemporaryData.unexpired.find_by(key: key)
+      end
+      session[key] = nil if use_session
       tmp_data.destroy if tmp_data
     end
 
-    def update_tmp_data(key, data)
-      tmp_data = TemporaryData.unexpired.find_by_id(session[key]) 
+    def update_tmp_data(key, data, use_session = false)
+      tmp_data = if use_session
+        TemporaryData.unexpired.find_by_id(session[key])
+      else
+        TemporaryData.unexpired.find_by(key: key)
+      end
+
       unless tmp_data.nil? 
         tmp_data.data = data
         tmp_data.save!
       else
-        session[key] = nil
+        session[key] = nil if use_session
       end
     end
   end
