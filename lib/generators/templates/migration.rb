ac5da8d91450ed907f0603fb4dfb4e0390e8c9b2
Vladimir <vulkan.u4u1@gmail.com>
2022-06-24 02:04:52 +0600

    Update migration.rb

--- lib/generators/templates/migration.rb
+++ lib/generators/templates/migration.rb
@@ -3,6 +3,7 @@ class CreateTemporaryData < ActiveRecord::Migration[5.0]
     create_table :temporary_data do |t|
       t.text :data
       t.datetime :expires_at
+      t.string :key, null: false
 
       t.timestamps
     end
